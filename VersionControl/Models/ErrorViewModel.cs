7de39f8d387f92bbc40f95590e9c93367e38a7c0
Paul Connolly <connolpe@mail.uc.edu>
2023-10-31 00:16:11 -0400

    Add project files.

---
+++ VersionControl/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace VersionControl.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
