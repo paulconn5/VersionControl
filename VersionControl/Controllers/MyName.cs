9a1c78287d2340047aa4b6b50797717101bea683
Paul Connolly <connolpe@mail.uc.edu>
2023-10-31 00:51:48 -0400

    Added MyName Controller

---
+++ VersionControl/Controllers/MyName.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace VersionControl.Controllers
+{
+    public class MyName : Controller
+    {
+        public IActionResult Index()
+        {
+            string name = "Paul Connolly";
+            return Json(name);
+        }
+    }
+}
